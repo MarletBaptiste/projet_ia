7d79a3f200e46d52303b2b0390e73afd089f65c2
MarletBaptiste <49087437+MarletBaptiste@users.noreply.github.com>
2019-03-30 06:36:43 +0100

    Update EscampeBoard.java

--- EscampeBoard.java
+++ EscampeBoard.java
@@ -1,4 +1,15 @@
 package modele;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.nio.file.Files;
+import java.nio.file.LinkOption;
+import java.io.FileReader;
+import java.io.FileNotFoundException;
+
 
 public class EscampeBoard {
 	
@@ -55,11 +66,6 @@ public class EscampeBoard {
 		array[4][5] = new Case(3);
 		array[5][5] = new Case(2);
 		
-		array[1][0] = new Case(2);
-		array[2][0] = new Case(2);
-		array[3][0] = new Case(1);
-		array[4][0] = new Case(2);
-		array[5][0] = new Case(2);	
 	}
 	
 	public boolean isValidMove(String move, String player) {
@@ -71,6 +77,73 @@ public class EscampeBoard {
 		}
 		return false;
 	}
+	public static void InitGameFromFile() throws IOException{
+		/*	Path path = Paths.get("/home/truffe/Eclipse_JAVA/Test.txt");
+		List<String> lignes = Files.readAllLines(path);
+		for(String ligne : lignes) {
+			System.out.println(ligne);
+			
+		}
+		*/
+	File file = new File("FichierDeSauvegarde");
+	//BufferedReader bufferedReader = null;
+	FileReader fileReader = null;
+	int numeroLigne = 0;
+	int numeroColonne = 0;
+	try {
+		fileReader = new FileReader(file);
+	//bufferedReader = new BufferedReader(fileReader);
 	
+	EscampeBoard e = new EscampeBoard();
+	char [] TableauDeSauvegarde = new char[36];
 	
+	fileReader.read(TableauDeSauvegarde);
+	System.out.println("voici la sauvegarde : ");
+	for(char c : TableauDeSauvegarde) {
+		
+		System.out.print(c);
+	}
+	for(char c : TableauDeSauvegarde) {
+		System.out.println("numero de la ligne : "+ numeroLigne+ " numero de la colonne "+ numeroColonne);
+		if(c =='N') {
+			Piece p = new Piece("Noir",true,false,e);
+			e.array[numeroColonne][numeroLigne].mettrePiece(p);
+		}
+		if(c == 'n') {
+			Piece p = new Piece("Noir",false,true,e);
+			e.array[numeroColonne][numeroLigne].mettrePiece(p);
+		}
+		if(c =='B') {
+			Piece p = new Piece("Blanc",true,false,e);
+			e.array[numeroColonne][numeroLigne].mettrePiece(p);
+		}
+		if(c =='b') {
+			Piece p = new Piece("Blanc",false,true,e);
+			e.array[numeroColonne][numeroLigne].mettrePiece(p);
+		}
+		numeroColonne = (numeroColonne+1)%6;
+		if(numeroColonne==0) {
+			numeroLigne = numeroLigne+1;
+		}
+		
+		
+	}
+	}catch(FileNotFoundException e) {
+		System.err.printf("le fichier %s n'a pas ete trouve,", file.toString());
+	}catch(IOException e) {
+		System.err.println("impossible de lire le contenue "+ file.toString());
+	}
+	try {
+		fileReader.close();
+	} catch (IOException e) {
+		System.err.println("impossible de fermer le fichier "+ file.toString());
+	} catch(NullPointerException e) {
+		System.err.print("impossible d'ouvrir le fichier");
+	}
+}
+	
+	public static void main(String[] args) throws IOException{
+		InitGameFromFile();
+	
+	}
 }
